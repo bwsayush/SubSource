77aebb2adb262db1fb6dc2e68e34e729a3c9eb94
bwsayush <bwsayush@gmail.com>
2021-02-25 16:37:10 +0530

    Build 1

---
+++ test-output-thread/report.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([
+  {
+    "id": "c7ffbd94-e2cf-434e-aaa1-325a6bc70a48",
+    "feature": "To test the Company functionality of Subsource",
+    "scenario": "Check Company deatails",
+    "start": 1614250738598,
+    "group": 13,
+    "content": "",
+    "tags": "",
+    "end": 1614250764517,
+    "className": "passed"
+  },
+  {
+    "id": "9340a6b1-18c6-491f-96dd-b7b508bc0ef3",
+    "feature": "To test the Shoppage functionality of Subsource",
+    "scenario": "Check Shop deatails",
+    "start": 1614250738525,
+    "group": 14,
+    "content": "",
+    "tags": "",
+    "end": 1614250769572,
+    "className": "passed"
+  }
+]);
+CucumberHTML.timelineGroups.pushArray([
+  {
+    "id": 13,
+    "content": "Thread[TestNG-PoolService-0,5,main]"
+  },
+  {
+    "id": 14,
+    "content": "Thread[TestNG-PoolService-1,5,main]"
+  }
+]);
+});
